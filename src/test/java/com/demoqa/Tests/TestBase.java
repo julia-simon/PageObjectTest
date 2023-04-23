838906f057b9df51ef432845380f6219c207e03a
Julia Simon <juli.simonova@gmail.com>
2023-04-23 22:49:20 +0300

    first commit

---
+++ src/test/java/com/demoqa/Tests/TestBase.java
@@ -0,0 +1,17 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = "Chrome";
+        Configuration.timeout = 5000;
+        Configuration.holdBrowserOpen = true;
+        Configuration.pageLoadStrategy="eager";
+    }
+}
