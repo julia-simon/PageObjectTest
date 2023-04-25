b11c83b8055563d3e0c233172798a1c3012062bc
Julia Simon <juli.simonova@gmail.com>
2023-04-26 01:03:50 +0300

    Added test data

--- src/test/java/com/demoqa/tests/TestFormWithPageObjectsTests.java
+++ src/test/java/com/demoqa/tests/TestFormWithPageObjectsTests.java
@@ -3,6 +3,8 @@ package com.demoqa.tests;
 import com.demoqa.pages.RegistrationPage;
 import org.junit.jupiter.api.Test;
 
+import static com.demoqa.tests.TestData.*;
+
 
 public class TestFormWithPageObjectsTests extends TestBase {
 
@@ -12,30 +14,30 @@ public class TestFormWithPageObjectsTests extends TestBase {
     void succesfullFillFormTests() {
         registrationPage.openPage()
                 .removeFooter()
-                .setFirstName("Chandler")
-                .setLastName("Bing")
-                .setEmail("bing@gmail.com")
-                .setGender("Male")
-                .setMobileNumber("8880000097")
-                .setBirthDate("8", "March", "1977")
-                .setSubject("History")
-                .setHobby("Music")
-                .uploadPicture("1.jpg")
-                .setCurrentAddress("My address")
-                .setState("NCR")
-                .setCity("Noida")
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setGender(genderUser)
+                .setMobileNumber(mobileNumber)
+                .setBirthDate(day, month, year)
+                .setSubject(subjects)
+                .setHobby(hobbies)
+                .uploadPicture(picture)
+                .setCurrentAddress(currentAddress)
+                .setState(state)
+                .setCity(city)
                 .clickSubmit();
 
         registrationPage.verifyRegistrationResultModalAppears()
-                .verifyResult("Student Name", "Chandler Bing")
-                .verifyResult("Student Email", "bing@gmail.com")
-                .verifyResult("Gender", "Male")
-                .verifyResult("Mobile", "8880000097")
-                .verifyResult("Date of Birth", "8 March,1977")
-                .verifyResult("Subjects", "History")
-                .verifyResult("Hobbies", "Music")
-                .verifyResult("Picture", "1.jpg")
-                .verifyResult("Address", "My address")
-                .verifyResult("State and City", "NCR Noida");
+                .verifyResult("Student Name", firstName + " " + lastName)
+                .verifyResult("Student Email", email)
+                .verifyResult("Gender", genderUser)
+                .verifyResult("Mobile", mobileNumber)
+                .verifyResult("Date of Birth", String.format("%s %s,%s", day, month, year))
+                .verifyResult("Subjects", subjects)
+                .verifyResult("Hobbies", hobbies)
+                .verifyResult("Picture", picture)
+                .verifyResult("Address", currentAddress)
+                .verifyResult("State and City", state + " " + city);
     }
 }
