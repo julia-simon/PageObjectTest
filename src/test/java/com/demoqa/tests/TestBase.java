9948e7f3d40755cf771de90eee107ea7448e02c9
Julia Simon <juli.simonova@gmail.com>
2023-05-21 20:12:08 +0300

    Changed TestBase

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.Map;
 
 
 public class TestBase {
@@ -23,13 +24,18 @@ public class TestBase {
         //Configuration.timeout = 5000;
         //Configuration.holdBrowserOpen = true;
         Configuration.pageLoadStrategy = "eager";
-        /*Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
         Configuration.browserCapabilities = capabilities;
-        WebDriverManager.chromedriver().setup();*/
     }
+
 
     @BeforeEach
     void addListener() {
